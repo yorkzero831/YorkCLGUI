3718aa2d612719b53ccff9960c0c247b4dc09b55
coly30 <coly30@coly30-no-Mac-mini.local>
2017-01-04 10:55:21 +0900

    update

--- YorkCLGUI/YorkWidgets/yorkDemo.cpp
+++ YorkCLGUI/YorkWidgets/yorkDemo.cpp
@@ -1,55 +1,55 @@
-//
-//
-//#include "YorkWindowsManager.h"
-//#include "YorkButton.h"
-//
-//using namespace YorkWidget;
-//
-//
-//
-//
-//
-//void buttonClick(YorkWidgets* e)
-//{
-//    printf("thisButtonClicked\n");
-//}
-//
-//
-//
-//int main(int, char**)
-//{
-//
-//    YorkWindow win = YorkWindow();
-//    
-//    YorkButton bt1 = YorkButton(ImVec2(100, 100), ImVec2(100, 100), "AAAAAAAA");
-//    bt1.SetClickedFunc(*buttonClick);
-//    
-//    YorkButton bt2 = YorkButton(ImVec2(200,150), ImVec2(50,20),"BBBBB");
-//    bt2.SetClickedFunc(*buttonClick);
-//    
-//    
-//    
-//    win.AddWidget(&bt1);
-//    
-//    win.AddWidget(&bt2);
-//
-//    win.DrawWindow();
-//    
-//    
-//
-//	return 0;
-//}
-//
-//#ifdef _WIN32
-//int CALLBACK WinMain(
-//	_In_  HINSTANCE hInstance,
-//	_In_  HINSTANCE hPrevInstance,
-//	_In_  LPSTR lpCmdLine,
-//	_In_  int nCmdShow)
-//{
-//	return main(0, 0);
-//}
-//
-//#endif
-//
-//
+
+
+#include "YorkWindowsManager.h"
+#include "YorkButton.h"
+
+using namespace YorkWidget;
+
+
+
+
+
+void buttonClick(YorkWidgets* e)
+{
+    printf("thisButtonClicked\n");
+}
+
+
+
+int main(int, char**)
+{
+
+    YorkWindow win = YorkWindow();
+    
+    YorkButton bt1 = YorkButton(ImVec2(100, 100), ImVec2(100, 100), "AAAAAAAA");
+    bt1.SetClickedFunc(*buttonClick);
+    
+    YorkButton bt2 = YorkButton(ImVec2(200,150), ImVec2(50,20),"BBBBB");
+    bt2.SetClickedFunc(*buttonClick);
+    
+    
+    
+    win.AddWidget(&bt1);
+    
+    win.AddWidget(&bt2);
+
+    win.DrawWindow();
+    
+    
+
+	return 0;
+}
+
+#ifdef _WIN32
+int CALLBACK WinMain(
+	_In_  HINSTANCE hInstance,
+	_In_  HINSTANCE hPrevInstance,
+	_In_  LPSTR lpCmdLine,
+	_In_  int nCmdShow)
+{
+	return main(0, 0);
+}
+
+#endif
+
+
